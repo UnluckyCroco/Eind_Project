e29f2b3c2fe720c6525f58ee2181fbe2ddd19f26
KoenAKASwimboy <koen.schreur@student.hu.nl>
2018-10-10 11:46:59 +0200

    hoiwereldofso

--- fietsenstalling.py
+++ fietsenstalling.py
@@ -1,7 +1,43 @@
+import random
 def fiets_registreren():
+    vnaam = input('Geef uw voornaam: ')
+    anaam = input('Geef uw tussenvoegsels en achternaam: ')
+    telefoon = input('Geef uw telefoonnummer: 06-')
+    while len(telefoon) is not 8: #checkt of het tefeloonnummer 8 tekens lang is
+        print('Telefoon nummer moet 8 cijfers bevatten')
+        telefoon = input('Geef uw telefoonnummer: 06-')
+    for x in range(1):
+     rnummer = random.randint(1,701)  #maakt een random nummer aan
+     infile = open('Gegevens', 'r')
+     regels = infile.readlines()
+     for regel in regels:
+        zin = regel.split(';')
+        nummer = zin[0]
+        if rnummer == nummer :  #checkt als het random nummer niet al bezet is
+            Random = True
+        else:
+            Random = False
+            print('Uw nummer is:', rnummer)
+            break
+        while Random:
+            fiets_registreren()
+     with open('Gegevens', 'a') as f:
+         f.write('\n')
+         f.write(str(rnummer))
+         f.write(';')
+         f.write(vnaam)
+         f.write(';')
+         f.write(anaam)
+         f.write(';')
+         f.write(telefoon)
 
-def fiets_stallen():
 
-def fiets_ophalen():
 
-def informatie():
+print(fiets_registreren())
+
+
+#def fiets_stallen():
+
+#def fiets_ophalen():
+
+#def informatie():
