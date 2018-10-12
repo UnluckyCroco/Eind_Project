96631b877c4db250e8c29c38b03cb5df9b70cfc8
KoenAKASwimboy <koen.schreur@student.hu.nl>
2018-10-12 09:32:26 +0200

    Registreren

--- fietsenstalling.py
+++ fietsenstalling.py
@@ -2,8 +2,9 @@ import random
 def fiets_registreren():
     vnaam = input('Geef uw voornaam: ')
     anaam = input('Geef uw tussenvoegsels en achternaam: ')
-    telefoon = input('Geef uw telefoonnummer: 06-')
-    if telefoon.isdigit():
+
+    telefoon = input('Geef uw telefoonnummer: 06-')  #telefoon
+    if telefoon.isdigit():  #checkt als telefoon wel uit nummers bestaat
         digit = telefoon
     else:
         digit = 'niks'
@@ -18,6 +19,24 @@ def fiets_registreren():
     while len(telefoon) is not 8: #checkt of het tefeloonnummer 8 tekens lang is
         print('Telefoon nummer moet 8 cijfers bevatten')
         telefoon = input('Geef uw telefoonnummer: 06-')
+
+    ov = input('Geef de laatste 4 nummers van uw ov-chipkaar: ') #OV-chipkaart
+    if ov.isdigit():  #checkt als ov wel uit nummers bestaat
+        nummer = ov
+    else:
+        nummer = 'niks'
+    while ov is not nummer:
+        print('OV nummer mag alleen cijfers bevatten')
+        telefoon = input('Geef de laatste 4 nummers van uw OV-chipkaart: ')
+        if ov.isdigit():
+            nummer = ov
+        else:
+            nummer = 'nikskhk'
+
+    while len(ov) is not 4: #checkt of het ov 4 tekens lang is
+        print('OV nummer moet 4 cijfers bevatten')
+        telefoon = input('Geef de laatse 4 nummers van uw OV-chipkaart: ')
+
     for x in range(1):
          rnummer = random.randint(1,701)  #maakt een random nummer aan
          infile = open('Gegevens', 'r')
