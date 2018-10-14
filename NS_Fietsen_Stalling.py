b4797327ee6854fceca387d25ea8bb0775aeaf79
IJuanTM <iwan2510@gmail.com>
2018-10-14 21:48:07 +0200

    Begin

--- NS_Fietsen_Stalling.py
+++ NS_Fietsen_Stalling.py
@@ -1,11 +1,10 @@
-import Registreren
-
 def inlog_menu():
     print('1: Registreren')
     print('2: Inloggen')
     optie = int(input('Kies een optie: '))
     if optie == 1:
-        print(Registreren.fiets_registreren())
+        from Registreren import fiets_registreren
+        print(fiets_registreren())
     else:
         exit()
 
