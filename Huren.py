d77497cea468fd85b28a25331ecc89cae70ce20c
Spyreex <daniel.phoeng@student.hu.nl>
2018-10-16 18:20:35 +0200

    klaar denk ik

--- Huren.py
+++ Huren.py
@@ -3,19 +3,22 @@ def fiets_huren():
     infile = open('Ingelogd','r')
     ilines = infile.readlines()
     for iline in ilines:
-        naam = iline.split(';')
-
+        inlog = iline.split(';')
+        inlogz = inlog[4].strip('\n')
     checkhuur = 0
 
     file = open('Huurgegevens', 'r')
     lines = file.readlines()
+    file.close()
 
-    for line in lines:
+    for line in lines:                                  # checked of dezelfde gegevens in 'Ingelogd' ook staan in 'Huurgegevens', zoja, dan betekend het dat er al een fiets is gehuurd
         x = line.split(';')
         if checkhuur == 0:
-            if naam[0] in x:
-                print('U heeft al een fiets gehuurd')
-                checkhuur = 1
+            if inlog[0] in x:
+                if inlog[1] in x:
+                    if inlogz in x:
+                        print('U heeft al een fiets gehuurd')
+                        checkhuur = 1
     if checkhuur == 0:
         tijdH = time.strftime('%H')  # uren
         tijdM = time.strftime('%M')  # minuten
@@ -26,7 +29,7 @@ def fiets_huren():
         tijddatum = tijdH + ':' + tijdM + ':' + tijdS + ' ' + datumd + '/' + datumm + '/' + datumY
         print('De fiets is gehuurd vanaf', tijddatum)
         huurfile = open('Huurgegevens', 'a')
-        huurfile.write(naam[0] + ';' + str(tijdH) + ';' + str(tijdM) + ';' + str(tijdS) + ';' + str(datumd) + ';' + str(datumm) + ';' + str(datumY) + '\n')
+        huurfile.write(inlog[0] + ';' + inlog[1] + ';' + inlogz + ';' + str(tijdH) + ';' + str(tijdM) + ';' + str(tijdS) + ';' + str(datumd) + ';' + str(datumm) + ';' + str(datumY) + '\n')
         huurfile.close()
 
 
--- Terugbrengen.py
+++ Terugbrengen.py
@@ -1,19 +1,34 @@
 import time
 
 
-def regel_verwijderen(naam):
+def regel_verwijderen():
     lijst = []
     file = open('Huurgegevens', 'r')
     regels = file.readlines()
     file.close()
+
+    infile = open('Ingelogd', 'r')
+    ilines = infile.readlines()
+    for iline in ilines:
+        inlog = iline.split(';')
+        inlogz = inlog[4].strip('\n')
+    infile.close()
+
     for regel in regels:
         x = regel.split(';')
-        if not naam[0] == x[0]:
+        counter = 0
+        if not inlog[0] == x[0]:
+            counter += 1
+        if not inlog[1] == x[1]:
+            counter += 1
+        if not inlogz == x[2]:
+            counter += 1
+        if counter > 0:
             lijst.append(regel)
     file.close()
     wfile = open('Huurgegevens','w')
-    for q in range(len(lijst)):
-        wfile.write(lijst[q])
+    for gegevens in range(len(lijst)):
+        wfile.write(lijst[gegevens])
 
 
 
@@ -28,75 +43,79 @@ def fiets_terugbrengen():
     infile = open('Ingelogd', 'r')
     ilines = infile.readlines()
     for iline in ilines:
-        naam = iline.split(';')
+        inlog = iline.split(';')
+        inlogz = inlog[4].strip('\n')
+    infile.close()
 
     for linet in linest:
         y = linet.split(';')
 
-        if naam[0] in y:
-            datumstartH = y[1]
-            datumstartM = y[2]
-            datumstartS = y[3]
-            datumstartd = y[4]
-            datumstartm = y[5]
-            datumstartY = y[6].strip('\n')
+        if inlog[0] in y:
+            if inlog[1] in y:
+                if inlogz in y:
+                    datumstartH = y[3]
+                    datumstartM = y[4]
+                    datumstartS = y[5]
+                    datumstartd = y[6]
+                    datumstartm = y[7]
+                    datumstartY = y[8].strip('\n')
 
-            tijdHt = time.strftime('%H')  # uren;tijdsbepaling van nu
-            tijdMt = time.strftime('%M')  # minuten
-            tijdSt = time.strftime('%S')  # seconden
-            datumdt = time.strftime('%d')  # dag
-            datummt = time.strftime('%m')  # maand
-            datumYt = time.strftime('%Y')  # jaar
+                    tijdHt = time.strftime('%H')  # uren;tijdsbepaling van nu
+                    tijdMt = time.strftime('%M')  # minuten
+                    tijdSt = time.strftime('%S')  # seconden
+                    datumdt = time.strftime('%d')  # dag
+                    datummt = time.strftime('%m')  # maand
+                    datumYt = time.strftime('%Y')  # jaar
 
-            tijddatum = tijdHt + ':' + tijdMt + ':' + tijdSt + ' ' + datumdt + '/' + datummt + '/' + datumYt
-            print('U heeft de fiets gehuurd sinds','{}:{}:{} {}/{}/{}'.format(datumstartH,datumstartM,datumstartS,datumstartd,datumstartm,datumstartY),'en de fiets terug gebracht op', tijddatum)
-            total = int(datumstartH)*3600 + int(datumstartM)*60 + int(datumstartS) + int(datumstartd)*dag + int(datumstartm)/12*jaar + int(datumstartY)*jaar
-            totalt = int(tijdHt)*3600 + int(tijdMt)*60 + int(tijdSt) + int(datumdt)*dag + int(datummt)/12*jaar + int(datumYt)*jaar
+                    tijddatum = tijdHt + ':' + tijdMt + ':' + tijdSt + ' ' + datumdt + '/' + datummt + '/' + datumYt
+                    print('U heeft de fiets gehuurd sinds','{}:{}:{} {}/{}/{}'.format(datumstartH,datumstartM,datumstartS,datumstartd,datumstartm,datumstartY),'en de fiets terug gebracht op', tijddatum)
+                    total = int(datumstartH)*3600 + int(datumstartM)*60 + int(datumstartS) + int(datumstartd)*dag + int(datumstartm)/12*jaar + int(datumstartY)*jaar
+                    totalt = int(tijdHt)*3600 + int(tijdMt)*60 + int(tijdSt) + int(datumdt)*dag + int(datummt)/12*jaar + int(datumYt)*jaar
 
-            gehuurdtijdtotal = totalt - total
-            gehuurdtijd = gehuurdtijdtotal
-            totalY = gehuurdtijd // (jaar)                       # hier en onder berekent hij per tijd hoelang de fiets gehuurd is
-            if totalY == 1:
-                jaren = 'jaar,'
-            else:
-                jaren = 'jaren,'
-            gehuurdtijd = gehuurdtijd - totalY * jaar
-            totalm = gehuurdtijd // (jaar / 12)
-            if totalm == 1:
-                maanden = 'maand,'
-            else:
-                maanden = 'maanden,'
-            gehuurdtijd = gehuurdtijd - totalm * jaar / 12
-            totald = gehuurdtijd // (dag)
-            if totald == 1:
-                dagen = 'dag,'
-            else:
-                dagen = 'dagen,'
-            gehuurdtijd = gehuurdtijd - totald * dag
-            totalH = gehuurdtijd // (3600)
-            if totalH == 1:
-                uren = 'uur,'
-            else:
-                uren = 'uren,'
-            gehuurdtijd = gehuurdtijd - totalH * 3600
-            totalM = gehuurdtijd // (60)
-            if totalM == 1:
-                minuten = 'minuut,'
-            else:
-                minuten = 'minuten,'
-            gehuurdtijd = gehuurdtijd - totalM * 60
-            totalS = gehuurdtijd
-            if totalS == 1:
-                seconden = 'seconde'
-            else:
-                seconden = 'seconden'
+                    gehuurdtijdtotal = totalt - total
+                    gehuurdtijd = gehuurdtijdtotal
+                    totalY = gehuurdtijd // (jaar)                       # hier en onder berekent hij per tijd hoelang de fiets gehuurd is
+                    if totalY == 1:
+                        jaren = 'jaar,'
+                    else:
+                        jaren = 'jaren,'
+                    gehuurdtijd = gehuurdtijd - totalY * jaar
+                    totalm = gehuurdtijd // (jaar / 12)
+                    if totalm == 1:
+                        maanden = 'maand,'
+                    else:
+                        maanden = 'maanden,'
+                    gehuurdtijd = gehuurdtijd - totalm * jaar / 12
+                    totald = gehuurdtijd // (dag)
+                    if totald == 1:
+                        dagen = 'dag,'
+                    else:
+                        dagen = 'dagen,'
+                    gehuurdtijd = gehuurdtijd - totald * dag
+                    totalH = gehuurdtijd // (3600)
+                    if totalH == 1:
+                        uren = 'uur,'
+                    else:
+                        uren = 'uren,'
+                    gehuurdtijd = gehuurdtijd - totalH * 3600
+                    totalM = gehuurdtijd // (60)
+                    if totalM == 1:
+                        minuten = 'minuut,'
+                    else:
+                        minuten = 'minuten,'
+                    gehuurdtijd = gehuurdtijd - totalM * 60
+                    totalS = gehuurdtijd
+                    if totalS == 1:
+                        seconden = 'seconde'
+                    else:
+                        seconden = 'seconden'
 
-            prijs = gehuurdtijdtotal//(60*15)*0.2 + 1           # elke 15 minuten + 20 cent en standaard 1 euro
-            print('U heeft de fiets voor',int(totalY),jaren,int(totalm),maanden,int(totald),dagen,int(totalH),uren,int(totalM),minuten,int(totalS),seconden,'gehuurd, dat kost u',str(prijs),'euro.')
+                    prijs = gehuurdtijdtotal//(60*15)*0.2 + 1           # elke 15 minuten + 20 cent en standaard 1 euro
+                    print('U heeft de fiets voor',int(totalY),jaren,int(totalm),maanden,int(totald),dagen,int(totalH),uren,int(totalM),minuten,int(totalS),seconden,'gehuurd, dat kost u',str(prijs),'euro.')
 
-            regel_verwijderen(naam)
+                    regel_verwijderen()
 
-            gelukt = 1                          # voor de "geen fiets gehuurd" dinges
+                    gelukt = 1                          # voor de "geen fiets gehuurd" dinges
     if gelukt != 1:
         print('U heeft geen fiets gehuurd')
     terugfile.close()
