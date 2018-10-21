3da6629399cfc5c2fd70159e74841e5a8fc070c9
KoenAKASwimboy <koen.schreur@student.hu.nl>
2018-10-21 11:04:00 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Gegevens
    #	fietsenstalling.py

--- test.py
+++ test.py
@@ -16,12 +16,12 @@ class Window(Tk):
                                  width=40)
         self.regibutton.place(relx=0.5, rely=0.5, anchor=SE)
 
-        self.logingobutton = Button(master=self, text='Inloggen',
+        self.logingobutton = Button(master=self, text='Inloggen', command=self.inloggen,
                                bg='lightgreen', relief=SOLID, font='Calibri',
                                width=40)
         self.logingobutton.place(relx=0.5, rely=0.5, anchor=SW)
 
-        self.infobutton = Button(master=self, text='Informatie', command=self.zien,
+        self.infobutton = Button(master=self, text='Informatie', command=self.infoalgemeen,
                             bg='lightgreen', relief=SOLID, font='Calibri',
                             width=40)
         self.infobutton.place(relx=0.5, rely=0.5, anchor=NE)
@@ -32,16 +32,11 @@ class Window(Tk):
         self.quitbutton.place(relx=0.5, rely=0.5, anchor=NW)
 
     def regi(self):
-        if self.telfield.get() and self.ovrfield.get() and self.vnaamfield.get() and self.anaamfield.get() and self.wwrfield.get() == '':
+        if self.telfield.get() or self.ovrfield.get() or self.vnaamfield.get() or self.anaamfield.get() or self.wwrfield.get() == '':
             return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-        if self.vnaamfield.get() == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-        if self.anaamfield.get() == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
+
         telefoon =self.telfield.get()
-        if telefoon == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-        elif telefoon.isdigit():  # TELEFOON
+        if telefoon.isdigit():  # TELEFOON
             digit = telefoon
         else:
             digit = ' '
@@ -52,9 +47,7 @@ class Window(Tk):
             return showwarning(title='Telefoon', message='Telefoonnummer moet 8 nummers bevatten')
 
         ov = self.ovrfield.get()
-        if ov == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-        elif ov.isdigit():  # OV
+        if ov.isdigit():  # OV
             nummer = ov
         else:
             nummer = ' '
@@ -65,21 +58,11 @@ class Window(Tk):
             return showwarning(title='OV', message='Telefoonnummer moet 4 nummers bevatten')
 
         ww = self.wwrfield.get()
-        if ww == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-
-        elif len(ww) < 6:
+        if len(ww) < 6:
             return showwarning(title='Wachtwoord', message='Het wachtwoord moet minstens 6 karakters bevatten')
-        else:
-            onnodig = 'onnodig'
 
-        ww2 = self.wwr2field.get()
-        if ww2 == '':
-            return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
-        elif self.wwrfield.get() != self.wwr2field.get():
+        if self.wwrfield.get() != self.wwr2field.get():
             return showwarning(title='Wachtwoord', message='De wachtwoorden zijn niet gelijk ')
-        else:
-            onnodig = 'onnodig'
 
         with open('Gegevens', 'a') as f:
 
@@ -138,10 +121,10 @@ class Window(Tk):
         self.ovrfield = Entry(master=self.top)
         self.ovrfield.grid(row=3, column=21, pady=20)
 
-        self.wwrfield = Entry(master=self.top)
+        self.wwrfield = Entry(master=self.top, show='*')
         self.wwrfield.grid(row=4, column=21, pady=20)
 
-        self.wwr2field = Entry(master=self.top)
+        self.wwr2field = Entry(master=self.top, show='*')
         self.wwr2field.grid(row=5, column=21, pady=20)
 
         self.registrerenbutton = Button(self.top, text='Registreren', command=self.regi,
@@ -169,12 +152,73 @@ class Window(Tk):
             vrij = 0
         return vrij
 
-    def zien(self):
+    def infoalgemeen(self):
         vrij = 'Om OV-fietsenstalling te gebruiken heeft u een geldige OV-chipkaart en telefoonnummer nodig. Er zijn geen stallingskosten, een fiets huren kost €240 per uur. Plaatsen vrij: '
         vrij2 = self.informatieIedereen()
         vrij3 = str(vrij)+ str(vrij2)
-        print(vrij3)
         showinfo(title='Informatie Algemeen', message=vrij3)
+
+    def clicked(self):
+        bericht = 'De ingevulde gegevens kloppen niet'
+        showwarning(title='popup', message=bericht)
+
+    def login(self):
+        #if self.naamfield.get() or self.ovfield.get() or self.wwfield.get() == '':
+           #return showwarning(title='Leeg', message='Vul alle velden in alstublieft')
+        file = open('Gegevens', 'r')
+        lines = file.readlines()
+        for line in lines:
+            y = line.strip('\n')
+            z = y.split(';')
+            if self.naamfield.get() == z[0]:
+                if self.ovfield.get() == z[4]:
+                    if self.wwfield.get() == z[2]:
+                        file.close()
+                        f1 = open('Ingelogd', 'a')
+                        f1.write(line)
+                        f1.close()
+                        return print('yes')
+
+        self.clicked()
+
+
+
+    def inloggen(self):
+        self.destroy()
+        self.top = Toplevel()
+        self.top.title("Inloggen")
+        self.top.geometry("1920x1080")
+
+        self.label = Label(self.top, text='Voornaam')
+        self.label.grid(row=0, column=20, ipadx=170, sticky=W)
+        self.label.config(font=("Calibri", 16))
+
+        self.label = Label(self.top, text='Wachtwoord')
+        self.label.grid(row=1, column=20, ipadx=170, sticky=W)
+        self.label.config(font=("Calibri", 16))
+
+        self.label = Label(self.top, text='Laatste 4 nummers van uw OV')
+        self.label.grid(row=2, column=20, ipadx=170, sticky=W)
+        self.label.config(font=("Calibri", 16))
+
+        self.naamfield = Entry(master=self.top)
+        self.naamfield.grid(row=0, column=21, pady=20)
+
+        self.wwfield = Entry(master=self.top, show='*')
+        self.wwfield.grid(row=1, column=21, pady=20)
+
+        self.ovfield = Entry(master=self.top)
+        self.ovfield.grid(row=2, column=21, pady=20)
+
+        self.loginbutton = Button(master=self.top, text='Inloggen', command=self.login,
+                             bg='lightgreen', relief=SOLID, font='Calibri',
+                             width=40)
+        self.loginbutton.place(relx=0.5, rely=0.5, anchor=SE)
+
+        self.quitbutton = Button(master=self.top, text='Afsluiten', command=quit,
+                            bg='red', relief=SOLID, font='Calibri',
+                            width=30)
+        self.quitbutton.place(relx=0.5, rely=0.5, anchor=SW)
 
 
 
