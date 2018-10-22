3739554042eb52c125dfefa534ecd60da5ae1492
olafwestra <olaf.westra@student.hu.nl>
2018-10-22 13:04:21 +0200

    Stalling

--- test.py
+++ test.py
@@ -299,33 +299,40 @@ class Window(Tk):
         stallenbericht = 'U heeft u fiets al veilig gestald op plek: ' + str(self.rnummer)
         showinfo(title='Stallen', message=stallenbericht)
 
-
-
     def fiets_stallen(self):
-        read = open('Gegevens', 'r')
+        read = open('Ingelogd', 'r')
+        read1 = open('Stallen.txt', 'r')
         write = open('Stallen.txt', 'a')
         infile = read.readlines()
+        infile1 = read1.readlines()
         outfile = write.write
+
+        for infiles1 in infile1:
+            zin1 = infiles1.split(';')
+            self.info1 = zin1[1]
 
         for infiles in infile:
             zin = infiles.split(';')
             self.info = zin[4]
 
-            #if info == naam:
+            if self.info == self.info1:
+                return self.warning()
+
+            # if info == naam:
             for x in range(1):
-                    self.rnummer = random.randint(1, 701)
-                    infile = open('Stallen.txt', 'r')
-                    regels = infile.readlines()
-                    for regel in regels:
-                        zin = regel.split(';')
-                        nummer = zin[2]
-                        ov = zin[1]
-                        if nummer == self.rnummer:
-                            self.fiets_stallen()
-                        if ov == self.info:
-                            self.warning()
-                    self.stallen = (str(self.times()) + '; ' + self.info + '; ' + str(self.rnummer) + '\n')
-                    outfile(self.stallen)
+                self.rnummer = random.randint(1, 701)
+                infile = open('Stallen.txt', 'r')
+                regels = infile.readlines()
+                for regel in regels:
+                    zin = regel.split(';')
+                    nummer = zin[2]
+                    ov = zin[1]
+                    if nummer == self.rnummer:
+                        self.fiets_stallen()
+                    if ov == self.info:
+                        self.warning()
+                self.stallen = (str(self.times()) + '; ' + self.info + '; ' + str(self.rnummer) + '\n')
+                outfile(self.stallen)
         return self.toonStallen()
 
     def regel_verwijderen(self):
